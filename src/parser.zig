b4a407db653cd8fcc26463ccc28839b2be6c532d
Gregory Mullen <_@gr.ht>
2025-06-06 08:49:55 -0700

    sort imports

--- src/parser.zig
+++ src/parser.zig
@@ -1,3 +1,3 @@
-pub const postfix = @import("parser/postfix.zig");
 pub const nginx = @import("parser/nginx.zig");
+pub const postfix = @import("parser/postfix.zig");
 pub const sshd = @import("parser/sshd.zig");
