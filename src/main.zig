7ae791187279d3adf87df01cc8c22384b1eec019
Gregory Mullen <_@gr.ht>
2025-06-17 09:51:41 -0700

    move Addr into net.zig

--- src/main.zig
+++ src/main.zig
@@ -467,72 +467,6 @@ fn meaningful(line: []const u8) ?Meaningful {
     return null;
 }
 
-pub const Addr = union(enum) {
-    ipv4: [4]u8,
-    ipv6: [16]u8,
-
-    fn getOct(comptime sep: u8, str: []const u8) !switch (sep) {
-        '.' => u8,
-        ':' => u16,
-        else => @compileError("not implemented"),
-    } {
-        switch (sep) {
-            '.' => {
-                var idx: usize = 0;
-                s: switch (str[idx]) {
-                    '0'...'9' => {
-                        idx += 1;
-                        if (idx < str.len) continue :s str[idx];
-                        continue :s '.';
-                    },
-                    '.', '\t', ' ' => return parseInt(u8, str[0..idx], 10),
-                    else => return error.InvalidAddr,
-                }
-            },
-            ':' => comptime unreachable,
-            else => comptime unreachable,
-        }
-    }
-
-    fn parseV4(str: []const u8) !Addr {
-        var rest: []const u8 = str;
-        const first = try getOct('.', rest);
-        rest = rest[1 + (indexOfScalar(u8, rest, '.') orelse return error.InvalidAddr) ..];
-        const second = try getOct('.', rest);
-        rest = rest[1 + (indexOfScalar(u8, rest, '.') orelse return error.InvalidAddr) ..];
-        const third = try getOct('.', rest);
-        rest = rest[1 + (indexOfScalar(u8, rest, '.') orelse return error.InvalidAddr) ..];
-        const forth = try getOct('.', rest);
-
-        return .{ .ipv4 = .{ first, second, third, forth } };
-    }
-
-    fn parseV6(str: []const u8) !Addr {
-        _ = str;
-        return .{ .ipv6 = unreachable };
-    }
-
-    pub fn parse(str: []const u8) !Addr {
-        if (indexOf(u8, str, ".")) |_| {
-            return try parseV4(str);
-        } else if (indexOf(u8, str, ":")) |_| {
-            return try parseV6(str);
-        } else return error.UnknownAddr;
-    }
-
-    pub fn format(
-        addr: Addr,
-        comptime _: []const u8,
-        _: std.fmt.FormatOptions,
-        w: anytype,
-    ) !void {
-        switch (addr) {
-            .ipv4 => |ip| return w.print("{}.{}.{}.{}", .{ ip[0], ip[1], ip[2], ip[3] }),
-            .ipv6 => unreachable,
-        }
-    }
-};
-
 const Timestamp = packed struct(i64) {
     ts: i64,
 
@@ -600,6 +534,8 @@ const parser = @import("parser.zig");
 const Event = @import("Event.zig");
 const Detection = @import("Detection.zig");
 const Actionable = @import("Actionable.zig");
+const net = @import("net.zig");
+pub const Addr = net.Addr;
 
 const std = @import("std");
 const Allocator = std.mem.Allocator;
