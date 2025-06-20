277e5265fdd6db65168e3cf26a446a74800a513a
Gregory Mullen <_@gr.ht>
2025-06-20 10:45:25 -0700

    move LogFile -> File.zig

---
+++ src/File.zig
@@ -0,0 +1,94 @@
+file: std.fs.File,
+src: union(enum) {
+    stdin: void,
+    fbs: std.io.FixedBufferStream([]const u8),
+},
+watch: bool,
+meta: std.fs.File.Metadata,
+line_buffer: [4096]u8 = undefined,
+
+const LogFile = @This();
+
+pub fn init(filename: []const u8, watch: bool) !LogFile {
+    const f = try std.fs.cwd().openFile(filename, .{});
+    const lf: LogFile = .{
+        .file = f,
+        .src = .{
+            .fbs = .{
+                .buffer = try mmap(f),
+                .pos = 0,
+            },
+        },
+        .watch = watch,
+        .meta = try f.metadata(),
+    };
+
+    return lf;
+}
+
+pub fn initStdin() !LogFile {
+    const in = std.io.getStdIn();
+    return .{
+        .file = in,
+        .src = .{
+            .stdin = {},
+        },
+        .watch = true,
+        .meta = try in.metadata(),
+    };
+}
+
+pub fn raze(lf: *LogFile) void {
+    lf.watch = false;
+    lf.file.close();
+    switch (lf.src) {
+        .fbs => |fbs| std.posix.munmap(@alignCast(fbs.buffer)),
+        else => {},
+    }
+}
+
+fn mmap(f: std.fs.File) ![]const u8 {
+    const PROT = std.posix.PROT;
+    const length = try f.getEndPos();
+    const offset = 0;
+    return std.posix.mmap(null, length, PROT.READ, .{ .TYPE = .SHARED }, f.handle, offset);
+}
+
+fn remap(lf: *LogFile) !void {
+    const meta = try lf.file.metadata();
+    if (meta.size() < lf.meta.size()) return error.Truncated;
+    if (meta.size() == lf.meta.size()) {
+        lf.meta = meta;
+        return;
+    }
+    lf.src.fbs.buffer = try std.posix.mremap(
+        @alignCast(@constCast(lf.src.fbs.buffer.ptr)),
+        lf.src.fbs.buffer.len,
+        meta.size(),
+        .{ .MAYMOVE = true },
+        null,
+    );
+    lf.meta = meta;
+}
+
+pub fn line(lf: *LogFile) !?[]const u8 {
+    switch (lf.src) {
+        .fbs => |*fbs| {
+            if (fbs.pos == fbs.buffer.len) try lf.remap();
+            var reader = fbs.reader();
+            return try reader.readUntilDelimiterOrEof(&lf.line_buffer, '\n');
+        },
+        .stdin => {
+            var pollfd: [1]std.os.linux.pollfd = .{.{
+                .fd = lf.file.handle,
+                .events = std.posix.POLL.IN,
+                .revents = 0,
+            }};
+            if (std.os.linux.poll(&pollfd, 1, 0) != 1) return null;
+            var reader = lf.file.reader();
+            return try reader.readUntilDelimiter(&lf.line_buffer, '\n');
+        },
+    }
+}
+
+const std = @import("std");
--- src/main.zig
+++ src/main.zig
@@ -23,100 +23,7 @@ fn usage(arg0: []const u8) noreturn {
     std.posix.exit(1);
 }
 
-const LogFile = struct {
-    file: std.fs.File,
-    src: union(enum) {
-        stdin: void,
-        fbs: std.io.FixedBufferStream([]const u8),
-    },
-    watch: bool,
-    meta: std.fs.File.Metadata,
-    line_buffer: [4096]u8 = undefined,
-
-    pub fn init(filename: []const u8, watch: bool) !LogFile {
-        const f = try std.fs.cwd().openFile(filename, .{});
-        const lf: LogFile = .{
-            .file = f,
-            .src = .{
-                .fbs = .{
-                    .buffer = try mmap(f),
-                    .pos = 0,
-                },
-            },
-            .watch = watch,
-            .meta = try f.metadata(),
-        };
-
-        return lf;
-    }
-
-    pub fn initStdin() !LogFile {
-        const in = std.io.getStdIn();
-        return .{
-            .file = in,
-            .src = .{
-                .stdin = {},
-            },
-            .watch = true,
-            .meta = try in.metadata(),
-        };
-    }
-
-    pub fn raze(lf: *LogFile) void {
-        lf.watch = false;
-        lf.file.close();
-        switch (lf.src) {
-            .fbs => |fbs| std.posix.munmap(@alignCast(fbs.buffer)),
-            else => {},
-        }
-    }
-
-    fn mmap(f: std.fs.File) ![]const u8 {
-        const PROT = std.posix.PROT;
-        const length = try f.getEndPos();
-        const offset = 0;
-        return std.posix.mmap(null, length, PROT.READ, .{ .TYPE = .SHARED }, f.handle, offset);
-    }
-
-    fn remap(lf: *LogFile) !void {
-        const meta = try lf.file.metadata();
-        if (meta.size() < lf.meta.size()) return error.Truncated;
-        if (meta.size() == lf.meta.size()) {
-            lf.meta = meta;
-            return;
-        }
-        lf.src.fbs.buffer = try std.posix.mremap(
-            @alignCast(@constCast(lf.src.fbs.buffer.ptr)),
-            lf.src.fbs.buffer.len,
-            meta.size(),
-            .{ .MAYMOVE = true },
-            null,
-        );
-        lf.meta = meta;
-    }
-
-    pub fn line(lf: *LogFile) !?[]const u8 {
-        switch (lf.src) {
-            .fbs => |*fbs| {
-                if (fbs.pos == fbs.buffer.len) try lf.remap();
-                var reader = fbs.reader();
-                return try reader.readUntilDelimiterOrEof(&lf.line_buffer, '\n');
-            },
-            .stdin => {
-                var pollfd: [1]std.os.linux.pollfd = .{.{
-                    .fd = lf.file.handle,
-                    .events = std.posix.POLL.IN,
-                    .revents = 0,
-                }};
-                if (std.os.linux.poll(&pollfd, 1, 0) != 1) return null;
-                var reader = lf.file.reader();
-                return try reader.readUntilDelimiter(&lf.line_buffer, '\n');
-            },
-        }
-    }
-};
-
-var file_buf: [64]LogFile = undefined;
+var file_buf: [64]File = undefined;
 var dryrun: bool = false;
 var exec_rules: bool = false;
 
@@ -133,7 +40,7 @@ pub fn main() !void {
     const arg0 = args.next() orelse usage("wat?!");
 
     // TODO 20 ought to be enough for anyone
-    var log_files: std.ArrayListUnmanaged(LogFile) = .initBuffer(&file_buf);
+    var log_files: std.ArrayListUnmanaged(File) = .initBuffer(&file_buf);
 
     var default_watch: bool = false;
     var quiet: bool = false;
@@ -329,7 +236,7 @@ fn printBanList(a: Allocator, stdout: std.io.AnyWriter, timeout: []const u8) !vo
     }
 }
 
-fn readFile(a: Allocator, logfile: *LogFile) !usize {
+fn readFile(a: Allocator, logfile: *File) !usize {
     var line_count: usize = 0;
 
     while (try logfile.line()) |line| {
@@ -484,6 +391,7 @@ const parser = @import("parser.zig");
 const Event = @import("Event.zig");
 const Detection = @import("Detection.zig");
 const Actionable = @import("Actionable.zig");
+const File = @import("File.zig");
 const net = @import("net.zig");
 pub const Addr = net.Addr;
 
